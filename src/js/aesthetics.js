11adaebe06e983deca480b2d170ef52f7c43d12b
Nathan Kolbow <nkolbow@wisc.edu>
2020-09-04 13:38:18 -0500

    Implemented filtering

--- src/js/aesthetics.js
+++ src/js/aesthetics.js
@@ -10,6 +10,143 @@ var DROP3 = "True Score";
 
 var DATA_TABLE;
 
+var FIRST_X_PER_DAY = 1;
+var LAST_X_PER_DAY = 1;
+var TOP_X_PER_DAY = 1;
+var BOTTOM_X_PER_DAY = 1;
+var TOP_X_ACC_PER_DAY = 1;
+var BOTTOM_X_ACC_PER_DAY = 1;
+var TOP_X_HYPER_PER_DAY = 1;
+var BOTTOM_X_HYPER_PER_DAY = 1;
+
+
+function update_filter_values() {
+    var temp_first = parseInt(document.getElementById('first-of-day').value) / 100;
+    var temp_last = parseInt(document.getElementById('last-of-day').value) / 100;
+    var temp_top = parseInt(document.getElementById('top-of-day').value) / 100;
+    var temp_bottom = parseInt(document.getElementById('bottom-of-day').value) / 100;
+    var temp_acc_top = parseInt(document.getElementById('top-acc-of-day').value) / 100;
+    var temp_acc_bottom = parseInt(document.getElementById('bottom-acc-of-day').value) / 100;
+    var temp_hyper_top = parseInt(document.getElementById('top-hyper-of-day').value) / 100;
+    var temp_hyper_bottom = parseInt(document.getElementById('bottom-hyper-of-day').value) / 100;
+
+    var refilter = (temp_first != FIRST_X_PER_DAY) || (temp_last != LAST_X_PER_DAY) || (temp_top != TOP_X_PER_DAY) || (temp_bottom != BOTTOM_X_PER_DAY) ||
+                   (temp_acc_top != TOP_X_ACC_PER_DAY) || (temp_acc_bottom != BOTTOM_X_ACC_PER_DAY) || (temp_hyper_top != TOP_X_HYPER_PER_DAY) || (temp_hyper_bottom != BOTTOM_X_HYPER_PER_DAY);
+
+    if(refilter) {
+        FIRST_X_PER_DAY = temp_first;
+        LAST_X_PER_DAY = temp_last;
+        TOP_X_PER_DAY = temp_top;
+        BOTTOM_X_PER_DAY = temp_bottom;
+        TOP_X_ACC_PER_DAY = temp_acc_top;
+        BOTTOM_X_ACC_PER_DAY = temp_acc_bottom;
+        TOP_X_HYPER_PER_DAY = temp_hyper_top;
+        BOTTOM_X_HYPER_PER_DAY = temp_hyper_bottom;
+
+        reload_data();
+    }
+}
+
+function filter(samples) {
+    // Input: list of scenario dicts
+    var k;
+    var ret = [];
+    for(var i in samples)
+        ret.push(samples[i]); // clone the list
+
+    var _last_entry_of_day = [];
+    var curr_day = ret[0].Time.Year * 10000 + ret[0].Time.Month * 100 + ret[0].Time.Day;
+    for(i in ret) {
+        var temp = ret[i].Time.Year * 10000 + ret[i].Time.Month * 100 + ret[i].Time.Day;
+        if(temp != curr_day)
+            _last_entry_of_day.push(i-1);
+
+        curr_day = temp;
+    }
+    _last_entry_of_day.push(i);
+
+    // Get the complex filters down to a point where they can be accomplished in one pass
+    // This simplifies removing objects from the return list greatly
+    i = 0;
+    var days = [];
+    for(var j in _last_entry_of_day) {
+        days.push([]);
+        for(k = i; k <= _last_entry_of_day[j]; k++) {
+            days[j].push({
+                        Score: ret[k].GeneralData.Score,
+                        Acc: ret[k].GeneralData.Accuracy,
+                        Hyperscore: ret[k].GeneralData.Hyperscore,
+                        Entry: k
+                      });
+        }
+
+        i = _last_entry_of_day[j] + 1;
+    }
+
+    var toremove = [];
+    // Sort the days by score and add the removeable indeces
+    for(j in days) {
+        days[j].sort((a, b) => (a.Score > b.Score) ? 1 : -1);
+        // Remove if the score is not in the top X%
+        for(k = 0; k < Math.floor(days[j].length * (1-TOP_X_PER_DAY)); k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+
+        // Remove if the score is not in the bottom X%
+        for(k = Math.ceil(days[j].length * BOTTOM_X_PER_DAY); k < days[j].length; k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+
+        days[j].sort((a, b) => (a.Acc > b.Acc) ? 1 : -1);
+        // Remove if the acc is not in the top X%
+        for(k = 0; k < Math.floor(days[j].length * (1-TOP_X_ACC_PER_DAY)); k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+
+        // Remove if the acc is not in the bottom X%
+        for(k = Math.ceil(days[j].length * BOTTOM_X_ACC_PER_DAY); k < days[j].length; k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+
+        days[j].sort((a, b) => (a.Hyperscore > b.Hyperscore) ? 1 : -1);
+        // Remove if the hyperscore is not in the top X%
+        for(k = 0; k < Math.floor(days[j].length * (1-TOP_X_HYPER_PER_DAY)); k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+
+        // Remove if the hyperscore is not in the bottom X%
+        for(k = Math.ceil(days[j].length * BOTTOM_X_HYPER_PER_DAY); k < days[j].length; k++) {
+            if(toremove.indexOf(days[j][k].Entry) == -1) toremove.push(days[j][k].Entry);
+        }
+    }
+
+    // Now everything is down to one pass so we can do it all together
+    i = 0;
+    for(j in _last_entry_of_day) {
+        var _ending = _last_entry_of_day[j];
+        // i is currently the starting spot for that day, ending is the end
+        // first remove based on FIRST_X_PER_DAY
+        for(k = i; k <= Math.floor((_ending - i) * (1-FIRST_X_PER_DAY) + i - 0.00001); k++) {
+            if(toremove.indexOf(k) == -1) toremove.push(k);
+        }
+
+        // second remove based on LAST_X_PER_DAY
+        for(k = Math.floor((_ending - i) * LAST_X_PER_DAY + i) + 1; k <= _ending; k++) {
+            if(toremove.indexOf(k) == -1) toremove.push(k);
+        }
+
+        i = _ending + 1;
+    }
+
+    // Sort in reverse, so we don't run into issues as we remove items
+    toremove.sort((a, b) => (a < b) ? 1 : -1);
+    for(i in toremove) {
+        ret.splice(toremove[i], 1);
+    }
+    console.log("Removing " + toremove.length + " items.")
+    return ret;
+}
+
 function update_charts() {
     var d1 = document.getElementById('chart1-select');
     var d2 = document.getElementById('chart2-select');
@@ -85,34 +222,34 @@ function redraw(chart, selected) {
     // No need to update labels b/c that will already have the correct amount of entries
     chart.data.datasets[0].data =[];
     chart.data.datasets[0].label = selected;
-     var list = JSON.parse(window.localStorage.getItem('challenge_data'))[document.getElementById('dropdown').value];
+     var list = filter(JSON.parse(window.localStorage.getItem('challenge_data'))[document.getElementById('dropdown').value]);
      for(var i in list) {
          chart.data.datasets[0].data.push(list[i].GeneralData[selected]);
      }
     chart.update();
 }
 
-function toggle_scaling() {
-    SCALE = (SCALE == 'linear') ? 'logarithmic' : 'linear';
-    CHART1.options.scales.yAxes[0].type = SCALE;
-    CHART2.options.scales.yAxes[0].type = SCALE;
-    CHART3.options.scales.yAxes[0].type = SCALE;
-    document.getElementById('toggle-scale').innerText = 'Score Axis Scale: ' + SCALE;
-    CHART1.update();
-    CHART2.update();
-    CHART3.update();
-}
-
-function toggle_max_state() {
-    MAX_STATE = !MAX_STATE;
-    CHART1.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
-    CHART2.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
-    CHART3.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
-    document.getElementById('toggle-max').innerText = 'Scale From Zero: ' + ((MAX_STATE) ? 'On' : 'Off');
-    CHART1.update();
-    CHART2.update();
-    CHART3.update();
-}
+// function toggle_scaling() {
+//     SCALE = (SCALE == 'linear') ? 'logarithmic' : 'linear';
+//     CHART1.options.scales.yAxes[0].type = SCALE;
+//     CHART2.options.scales.yAxes[0].type = SCALE;
+//     CHART3.options.scales.yAxes[0].type = SCALE;
+//     document.getElementById('toggle-scale').innerText = 'Score Axis Scale: ' + SCALE;
+//     CHART1.update();
+//     CHART2.update();
+//     CHART3.update();
+// }
+//
+// function toggle_max_state() {
+//     MAX_STATE = !MAX_STATE;
+//     CHART1.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
+//     CHART2.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
+//     CHART3.options.scales.yAxes[0].ticks.beginAtZero = MAX_STATE;
+//     document.getElementById('toggle-max').innerText = 'Scale From Zero: ' + ((MAX_STATE) ? 'On' : 'Off');
+//     CHART1.update();
+//     CHART2.update();
+//     CHART3.update();
+// }
 
 function post_file_collection() {
     document.getElementById('pre-folder-items').remove();
@@ -135,10 +272,10 @@ function update_dropdown() {
         dropdown.appendChild(opt);
     }
 
-    update_chart();
+    reload_data();
 }
 
-function update_chart() {
+function reload_data() {
     // Reset chart data
     CHART1.data.labels = [];
     CHART1.data.datasets[0].data = [];
@@ -152,7 +289,7 @@ function update_chart() {
     var dropdown = document.getElementById('dropdown');
     var scenario = dropdown[dropdown.selectedIndex].text;
     var challenge_data = JSON.parse(window.localStorage.getItem('challenge_data'));
-    var list = challenge_data[scenario];
+    var list = filter(challenge_data[scenario]);
 
     var max_score = -1;
     var _max_score = -1;
@@ -172,16 +309,16 @@ function update_chart() {
         data = list[j].GeneralData;
 
         // Add the data to the chart
-        CHART1.data.labels.push(list[j].DateStamp);  // we need labels for placeholders, even though we don't use them
-        CHART2.data.labels.push(list[j].DateStamp);
-        CHART3.data.labels.push(list[j].DateStamp);
+        CHART1.data.labels.push(list[j].Time.DateStamp);  // we need labels for placeholders, even though we don't use them
+        CHART2.data.labels.push(list[j].Time.DateStamp);
+        CHART3.data.labels.push(list[j].Time.DateStamp);
         CHART1.data.datasets[0].data.push(data[DROP1]);
         CHART2.data.datasets[0].data.push(data[DROP2]);
         CHART3.data.datasets[0].data.push(data[DROP3]);
 
         // Update the data table
         DATA_TABLE.addData({
-            Date: list[j].DateStamp,
+            Date: list[j].Time.DateStamp,
             Score: list[j].GeneralData.Score,
             Accuracy: list[j].GeneralData.Accuracy,
             'True Score': list[j].GeneralData['True Score'],
--- src/js/worker.js
+++ src/js/worker.js
@@ -10,30 +10,37 @@ onmessage = function(e) {
     for(var i in e.data) {
         var file = e.data[i];
         var path;
+        var split;
 
-        try {
-            if(file.path.includes('\\'))
-                path = file.path.split('\\');
-            else
-                path = file.path.split('/');
-        } catch(error) {
-            path = file.webkitRelativePath.split('/');
-            requisite_path_entires = [ "stats" ];
-        }
-
-        for(var j in requisite_path_entires) {
-            if(path[path.length - j - 2] != requisite_path_entires[requisite_path_entires.length-j-1]) {
-                tosend.Error = "badpath";
-                tosend.ErrorMessage = "Incorrect path chosen.  Trying selecting a path that looks like \".../steamapps/common/FPSAimTrainer/FPSAimTrainer/stats/\"";
-                postMessage(tosend);
-                return;
+        // If the lastModified field == 0 then the file is a sample file; don't bother checking its path
+        if(file.lastModified != 0) {
+            try {
+                if(file.path.includes('\\'))
+                    path = file.path.split('\\');
+                else
+                    path = file.path.split('/');
+            } catch(error) {
+                path = file.webkitRelativePath.split('/');
+                requisite_path_entires = [ "stats" ];
             }
-        }
 
+            for(var j in requisite_path_entires) {
+                if(path[path.length - j - 2] != requisite_path_entires[requisite_path_entires.length-j-1]) {
+                    tosend.Error = "badpath";
+                    tosend.ErrorMessage = "Incorrect path chosen.  Trying selecting a path that looks like \".../steamapps/common/FPSAimTrainer/FPSAimTrainer/stats/\"";
+                    postMessage(tosend);
+                    return;
+                }
+            }
+
+            split = file.webkitRelativePath.split('/');
+        } else {
+            split = [ 'stats/', file.name ];
+        }
         var fr = new FileReaderSync();
         var stats_dict = process_stats(fr.readAsText(file));
 
-        var split = file.webkitRelativePath.split('/');
+
         split = split[split.length - 1].split(' - Challenge - ');
         if(!unique_challenges.includes(split[0])) {
             challenge_data[split[0]] = [];
@@ -50,14 +57,21 @@ onmessage = function(e) {
         }
         date = new Date(full_list[0], full_list[1]-1, full_list[2],
                         full_list[3], full_list[4], full_list[5]);
-        stats_dict.Epoch = date.getTime();
-        stats_dict.DateStamp = date.toLocaleString();
+        stats_dict.Time = {};
+        stats_dict.Time.Epoch = date.getTime();
+        stats_dict.Time.DateStamp = date.toLocaleString();
+        stats_dict.Time.Year = date.getFullYear();
+        stats_dict.Time.Month = date.getMonth();
+        stats_dict.Time.Day = date.getDay();
+        stats_dict.Time.Hours = date.getHours();
+        stats_dict.Time.Minutes = date.getMinutes();
+        stats_dict.Time.Seconds = date.getSeconds();
 
         challenge_data[split[0]].push(stats_dict);
     }
 
     for(var item in challenge_data) {
-        challenge_data[item].sort((a, b) => (a.Epoch > b.Epoch) ? 1 : -1);
+        challenge_data[item].sort((a, b) => (a.Time.Epoch > b.Time.Epoch) ? 1 : -1);
     }
     tosend.unique_challenges = unique_challenges;
     tosend.challenge_data = challenge_data;
@@ -125,7 +139,7 @@ function process_stats(filetext) {
     }
     dict.GeneralData.Accuracy = (hits/shots).toFixed(2);
     dict.GeneralData['True Score'] = (dict.GeneralData.Score / dict.GeneralData.Accuracy).toFixed(2);
-    dict.GeneralData.Hyperscore = (dict.GeneralData.Score * Math.pow(dict.GeneralData.Accuracy, 2));
+    dict.GeneralData.Hyperscore = (dict.GeneralData.Score * Math.pow(dict.GeneralData.Accuracy, 2)).toFixed(2);
 
     dict.Settings = {};
     _line++;
